e0ab70a8e34234a1d56552d82a8022ef16f19f94
DISCORDBBBB <60780527+BSGreatuser@users.noreply.github.com>
2020-11-20 21:44:14 +0900

    Update ranks.py

--- ranks.py
+++ ranks.py
@@ -21,13 +21,21 @@ async def on_message(message):
     if message.content.startswith('!실시간검색어') or message.content.startswith('!실검'):
         json = requests.get('https://www.naver.com/srchrank?frm=main').json()
         ranks = json.get("data")
-
+        data = []
         for r in ranks:
             rank = r.get("rank")
             keyword = r.get("keyword")
-            await message.channel.send(str(rank) + ". " + str(keyword))
-            if rank > 4:
+            if rank > 10:
                 break
+            data.append(f'**{rank}**위 {keyword}')
+
+        dat = str(data)
+        dat = dat.replace("'","")
+        dat = dat.replace(", ","\n")
+        dat = dat[1:-1]
+        print(dat)
+        embed = discord.Embed(title= '네이버 실시간 검색어 순위', description=(dat),colour=0x19CE60)
+        await message.channel.send(embed=embed)
 
 
 client.run('★TOKEN★')
